9ecbda469879a0d8e0b4dc5c2834775142c50833
Matthew Tao <s134584@gapps.dilworth.school.nz>
2020-10-30 01:48:54 +0000

    remove redundant header change

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var scrolledTop = 10;
 $(document).scroll(function() { 
    if($(window).scrollTop() === 0) {
         $("header").css("height", "5em");
-        $("header").css("background-color", "#7E57BF");
         $(".logo-text").css("letter-spacing", scrolledTop + "px");
         $(".logo-img").css("transform", "scaleX(1)");
         if (scrolledTop < 70) {
@@ -14,7 +13,6 @@ $(document).scroll(function() {
    }
    else {
         $("header").css("height", "3em");
-        $("header").css("background-color", "#7E57BF");     
         $(".logo-text").css("letter-spacing", "3px");
         $(".logo-img").css("transform", "scaleX(-1)")
    }
